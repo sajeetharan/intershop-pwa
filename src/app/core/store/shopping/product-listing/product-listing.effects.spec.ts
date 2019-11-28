1cc3fef4d3cbd1c1f2cddaebcf19e772ebc49b24
Sebastian Glaß <s.glass@intershop.de>
2019-11-28 10:51:02 +0100

    changed searchParameter from base 64 to parameterlist

--- src/app/core/store/shopping/product-listing/product-listing.effects.spec.ts
+++ src/app/core/store/shopping/product-listing/product-listing.effects.spec.ts
@@ -139,9 +139,9 @@ describe('Product Listing Effects', () => {
           page: undefined
         [Shopping] Load Products For Filter:
           id: {"type":"search","value":"term","filters":"blablubb"}
-          searchParameter: "YmxhYmx1YmI="
+          searchParameter: "blablubb"
         [Shopping] Apply Filter:
-          searchParameter: "YmxhYmx1YmI="
+          searchParameter: "blablubb"
       `);
     }));
 
@@ -158,9 +158,9 @@ describe('Product Listing Effects', () => {
           page: undefined
         [Shopping] Load Products For Filter:
           id: {"type":"category","value":"cat","filters":"blablubb"}
-          searchParameter: "YmxhYmx1YmI="
+          searchParameter: "blablubb"
         [Shopping] Apply Filter:
-          searchParameter: "YmxhYmx1YmI="
+          searchParameter: "blablubb"
       `);
     }));
   });
